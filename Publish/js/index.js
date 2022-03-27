032964907a2d2a2b7696ebe26975192ba18d76cc
Gizmo-Verindipencil <gizmo.verindipencil.19920804@gmail.com>
2022-03-27 11:59:52 +0900

    Add display during name search

--- Publish/js/index.js
+++ Publish/js/index.js
@@ -27,9 +27,18 @@ document.addEventListener("DOMContentLoaded", e => {
         selectedColor = color;
     });
 
-    // カラーピッカーが閉じられるとき、選択色を反映
+    // カラーピッカーを閉じたときの処理
     picker.on("hide", instance => {
+        // 選択色を反映
         instance.applyColor(selectedColor);
+
+        // 結果の設定処理
+        const setResult = html => {
+            $(".result-board.body").html(html);
+        }
+
+        // 検索中を表示
+        setResult("<p>finding...</p>");
     });
 
     // 境界の落雨アニメーションを設定
