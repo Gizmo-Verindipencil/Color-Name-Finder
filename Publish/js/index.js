07b7f2a3f101b92a68e84a1dc145fcdcb7d1afda
Gizmo-Verindipencil <gizmo.verindipencil.19920804@gmail.com>
2022-03-27 17:57:11 +0900

    Add color name search process

--- Publish/js/index.js
+++ Publish/js/index.js
@@ -1,3 +1,7 @@
+// 全ての色情報を保持
+const gateway = new Gateway();
+const colors = gateway.getColors();
+
 // ドキュメントの準備完了後に実行
 document.addEventListener("DOMContentLoaded", e => {
     // カラーピッカーを初期化
@@ -39,6 +43,51 @@ document.addEventListener("DOMContentLoaded", e => {
 
         // 検索中を表示
         setResult("<p>finding...</p>");
+
+        // 結果テーブルの作成処理
+        const createTable = rows => {
+            return `<table>
+                <tr>
+                    <th>Category</th>
+                    <th>Name</th>
+                    <th>Phonetic</th>
+                    <th>Hex</th>
+                    <th>Score</th>
+                </tr>
+                ${rows.join("")}
+            </table>`;
+        }
+
+        // 結果テーブル行の作成処理
+        const createRow = color => {
+            return `<tr>
+                <td>${color.category}</td>
+                <td>${color.name}</td>
+                <td>${color.phonetic}</td>
+                <td>${color.hex}</td>
+                <td>${color.getDifference(hex)}</td>
+            </tr>`
+        }
+
+        // 一致する色があれば表示
+        const hex = selectedColor.toHEXA().toString().toLowerCase();
+        const matchedColors = colors.filter(x => x.hex === hex);
+        if (matchedColors.length > 0) {
+            const rows = [];
+            for(let i = 0; i < matchedColors.length; i++) {
+                rows.push(createRow(matchedColors[i]));
+            }
+            setResult(createTable(rows));
+            return;
+        }
+
+        // 類似する色を表示
+        colors.sort((a, b) => b.getDifference(hex) - a.getDifference(hex));
+        let top5 = [];
+        for(let i = 0; i < 5; i++) {
+            top5.push(createRow(colors[i]));
+        }
+        setResult(createTable(top5));
     });
 
     // 境界の落雨アニメーションを設定
